20b3fbb2f3c246ab748cc3c01a3d8bf8edaefe4d
Juan Camilo Vega <juancvegao@iMac-de-Juan.local>
2022-10-27 18:34:52 +1100

    Fixing port 3001

--- index_express.js
+++ index_express.js
@@ -39,6 +39,11 @@ const persons = [
   ]
 
 app.get('/', (req, res)=> {
+  console.log("ALGO")
    res.json(persons)
 })
 
+const port = process.env.PORT || 3001
+app.listen(port, () => {
+console.log(`Server running on port ${port}`)
+})

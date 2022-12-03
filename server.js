61ab30df2fb10f0d51b71dbfca56d71b2a135ea3
Juan Camilo Vega <juancvegao@iMac-de-Juan.local>
2022-12-03 15:28:06 +1100

    fixing scripts

--- server.js
+++
@@ -1,7 +0,0 @@
-
-if (process.env.NODE_ENV === 'production') {
-    //*Set static folder up in production
-    app.use(express.static('client/build'));
-
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-  }

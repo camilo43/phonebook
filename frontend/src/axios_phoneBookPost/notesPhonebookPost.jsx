98ae0e9ccf46ad836e561de208ba5681f746907b
Juan Camilo Vega <juancvegao@iMac-de-Juan.local>
2022-12-08 15:24:46 +1100

    Adding build to package.json. Getting ready to deply frontend

--- frontend/src/axios_phoneBookPost/notesPhonebookPost.jsx
+++ frontend/src/axios_phoneBookPost/notesPhonebookPost.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = 'http://localhost:3001/'
+const url = 'https://example-backend2.onrender.com/'
 
 const getting = () => { 
     console.log(">>Getting AXIOS")  

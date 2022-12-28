1af149e86741aa85cc338121d6049758f34fe526
Juan Camilo Vega <juancvegao@iMac-de-Juan.local>
2022-12-28 18:11:23 +1100

    Fixing issues found in Axios module

--- frontend/src/axios_phoneBookPost/notesPhonebookPost.jsx
+++ frontend/src/axios_phoneBookPost/notesPhonebookPost.jsx
@@ -1,5 +1,4 @@
-import axios from "axios";
-import {toast} from "react-toastify"
+import axios from 'axios'
 
 const url = 'https://example-backend2.onrender.com/'
 
@@ -8,26 +7,26 @@ const url = 'https://example-backend2.onrender.com/'
 //         toast.error("Please add a name")
 //     }
 // })
-const getting = () => { 
-    console.log(">>Getting AXIOS")  
-    const axiosBase = axios.get(`${url}api/data`)
-    return  axiosBase.then(resolve=>resolve.data)
+const getting = () => {
+  console.log('>>Getting AXIOS')
+  const axiosBase = axios.get(`${url}api/data`)
+  return  axiosBase.then(resolve => resolve.data)
 }
 
 const posting = (objectBase) => {
-    const axiosBase = axios.post(`${url}api/data`, objectBase)
-    const validation = axiosBase.then(resolve => resolve.data)
-    return validation
+  const axiosBase = axios.post(`${url}api/data`, objectBase)
+  const validation = axiosBase.then(resolve => resolve.data)
+  return validation
 }
 
 const deleting = (id) => {
-    const axiosBase = axios.delete(`${url}api/data/${id}`)
-    return axiosBase
+  const axiosBase = axios.delete(`${url}api/data/${id}`)
+  return axiosBase
 }
 
 const putting = (id, cambio) => {
-    console.log(">>PUTTING AXIOS")
-    const axiosBase = axios.put(`${url}api/data/${id}`, cambio)
-    return axiosBase
+  console.log('>>PUTTING AXIOS')
+  const axiosBase = axios.put(`${url}api/data/${id}`, cambio)
+  return axiosBase
 }
 export const notesPhonebook = { getting, posting, deleting, putting }
